f14f9a64561c7f59c3e1d55aa435a3397ff52c48
Ritalin <kazu.ktz.tamura@gmail.com>
2019-10-12 22:02:29 +0900

    [Fix] 第2ステージの読み込みを試みる(失敗)

--- src/08_stage_2/boot.s
+++ src/08_stage_2/boot.s
@@ -29,13 +29,24 @@ ipl:
         mov [BOOT.DRIVE], dl
 
         cdecl puts, .s0
-        cdecl reboot            ; 戻ってこない
-        cdecl puts, .s1
 
+        ; ** 第2ステージのロード **
+        mov ah, 0x02            ; 読み込み命令
+        mov al, 1               ; 読み込みセクタ数
+        mov cx, 0x0002          ; シリンダ / セクタ位置
+        mov dh, 0x00            ; ヘッド位置
+        mov dl, [BOOT.DRIVE]    ; ドライブ番号
+        mov bx, BOOT_LOAD + 512 ; 展開先アドレス
+        int 0x13
+        jnc .LOAD_2ND_STAGE_SUCCESS
+        cdecl puts, .err0
+        cdecl reboot            ; 再起動
+.LOAD_2ND_STAGE_SUCCESS:
         jmp $
 
 .s0:    db "Booting...", 0x0A, 0x0D, 0   
-.s1:    db "(STOPPED)", 0x0A, 0x0D, 0      
+.err0:  db "ERROR: Sector Read", 0x0A, 0x0D, 0   
+
 ALIGN 2, db 0
 BOOT:
 .DRIVE:
