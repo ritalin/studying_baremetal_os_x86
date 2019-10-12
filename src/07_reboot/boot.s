ef1bd18177cd8185bbb35d88720b924cadecb114
Ritalin <kazu.ktz.tamura@gmail.com>
2019-10-12 21:40:00 +0900

    [Fix] リブートの実装まで

--- src/07_reboot/boot.s
+++ src/07_reboot/boot.s
@@ -29,17 +29,20 @@ ipl:
         mov [BOOT.DRIVE], dl
 
         cdecl puts, .s0
-        
+        cdecl reboot            ; 戻ってこない
+        cdecl puts, .s1
+
         jmp $
 
-.s0:
-        db "Booting...", 0x0A, 0x0D, 0        
+.s0:    db "Booting...", 0x0A, 0x0D, 0   
+.s1:    db "(STOPPED)", 0x0A, 0x0D, 0      
 ALIGN 2, db 0
 BOOT:
 .DRIVE:
         dw 0
 
 %include "modules/real/puts.s"
+%include "modules/real/reboot.s"
 
 ;********************************************************************************
 ; ブートフラグ
