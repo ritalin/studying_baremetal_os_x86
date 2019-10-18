e74c8aef3159b6589cd9e732f020149f71050973
Ritalin <kazu.ktz.tamura@gmail.com>
2019-10-19 01:33:58 +0900

    [Fix] 16本のカラーバーを出力させるよう修正した

--- src/modules/protect/draw_color_bar.s
+++ src/modules/protect/draw_color_bar.s
@@ -20,12 +20,18 @@ draw_color_bar:
         mov esi, [ebp + 8]              ; 列
         mov edi, [ebp + 12]             ; 行
 
-        mov ecx, 2
+        mov ecx, 0
+.LOOP:
+        cmp ecx, 16                     ; 最大16本のカラーバーを出力
+        je .END
 
         mov eax, ecx
+        and eax, 0x01                   ; 0, 1を繰り返す    
+        shl eax, 3                      ; 1本あたり8文字占有するため                 
         add eax, esi
 
         mov ebx, ecx
+        shr ebx, 1                      ; 2回に1回incする
         add ebx, edi
 
         mov edx, ecx
@@ -35,6 +41,7 @@ draw_color_bar:
         cdecl draw_str, eax, ebx, edx, .format
 
         inc ecx
+        jmp .LOOP
 .END:
 
 ;**** レジスタの復帰 **** 
@@ -55,3 +62,8 @@ draw_color_bar:
         dw 0x0000, 0x0800
         dw 0x0100, 0x0900
         dw 0x0200, 0x0A00
+        dw 0x0300, 0x0B00
+        dw 0x0400, 0x0C00
+        dw 0x0500, 0x0D00
+        dw 0x0600, 0x0E00
+        dw 0x0700, 0x0F00
