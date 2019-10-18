f49255e1ca75231d1c043b6adc58e20aef3644e2
Ritalin <kazu.ktz.tamura@gmail.com>
2019-10-18 23:22:32 +0900

    [Add] copy to src_20

--- Makefile
+++ Makefile
@@ -13,8 +13,12 @@ PROGRAM_LIST := $(patsubst %.img,%.list,$(PROGRAM))
 all: ;
 
 .PHONY: run
-run: src_19
+run: src_20
 	$(VM) -monitor stdio $(PROGRAM)
+
+.PHONY: src_20
+src_20: 
+	$(MAKE) -B $(PROGRAM) PROJECT=$(SRC_DIR)/20_draw_str
 
 .PHONY: src_19
 src_19: 
---
+++ src/20_draw_str/kernel.s
@@ -0,0 +1,36 @@
+%include "include/define.s"
+%include "include/macro.s"
+
+        ORG KERNEL_LOAD
+
+[BITS 32]
+;********************************************************************************
+; エントリポイント
+;********************************************************************************
+kernel:
+        ; ** フォントアドレスを取得する **
+        mov esi, BOOT_LOAD + SECT_SIZE      ; ESI = 0x7c00 + 512
+        movzx eax, word [esi + font.seg]    ; フォントセグメント
+        movzx ebx, word [esi + font.off]    ; フォントオフセットアドレス
+        shl eax, 4                          ; リアルモードセグメントアドレスのため
+        add eax, ebx                        ; 実アドレスを計算
+        mov [FONT], eax
+
+        ; ** フォントをを印字
+        cdecl draw_font, 63, 13
+
+        jmp $
+
+ALIGN 4, db 0
+FONT:   dd 0                                ; フォントアドレス保持先   
+
+%include "modules/protect/vga.s"
+%include "modules/protect/draw_char.s"
+%include "modules/protect/draw_font.s"
+
+;********************************************************************************
+; パディング(8kB)
+;********************************************************************************
+        times KERNEL_SIZE - ($-$$) db 0x00
+
+
