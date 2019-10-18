42ec57618e025f29f49bafef5dd9467b7358bc91
Ritalin <kazu.ktz.tamura@gmail.com>
2019-10-18 22:56:35 +0900

    [Fix] フォント一覧の出力まで

--- src/19_draw_font/kernel.s
+++ src/19_draw_font/kernel.s
@@ -16,47 +16,8 @@ kernel:
         add eax, ebx                        ; 実アドレスを計算
         mov [FONT], eax
 
-        ; ** 8Bitの横線を描画
-        cdecl select_vga_write_plane, 0x07      ; 書き込みプレーンを指示 (_____RGB)
-        mov [0x000A_0000 + 400], byte 0xFF
-
-        cdecl select_vga_write_plane, 0x04      ; 書き込みプレーンを指示 (_____R__)
-        mov [0x000A_0000 + 480 + 1], byte 0xFF
-
-        mov [0x000A_0000 + 480 + 2], byte 0xFF
-
-        cdecl select_vga_write_plane, 0x01      ; 書き込みプレーンを指示 (_______B)
-        mov [0x000A_0000 + 480 + 3], byte 0xFF
-        
-        cdecl select_vga_write_plane, 0x02      ; 書き込みプレーンを指示 (______G_)
-
-        ; ** 画面を横切る横線を描画
-
-        lea     edi, [0x000A_0000 + 960]        ; EDI = VRAMアドレス;        
-        mov     ecx, 80                         ; ECX = 繰り返し回数;        
-        mov     al, 0xFF                        ; AL  = ビットパターン;        
-    rep stosb                                   ; *EDI++ = AL;
-
-        ; ** 8ドットの矩形を描画
-        mov edi, 1
-        shl edi, 8
-        lea edi, [edi * 4 + edi + 0xA_0000]     ; 10Bitシフト + 8Bitシフト = フォント1行分のオフセット
-        mov [edi + 80 * 0], word 0xFF
-        mov [edi + 80 * 1], word 0xFF
-        mov [edi + 80 * 2], word 0xFF
-        mov [edi + 80 * 3], word 0xFF
-        mov [edi + 80 * 4], word 0xFF
-        mov [edi + 80 * 5], word 0xFF
-        mov [edi + 80 * 6], word 0xFF
-        mov [edi + 80 * 7], word 0xFF
-
-        ; ** 文字を印字
-        cdecl draw_char, 1, 2, 0x010F, 'A'
-        cdecl draw_char, 2, 2, 0x010F, 'B'
-        cdecl draw_char, 3, 2, 0x010F, 'C'      ; ___B _I___
-        cdecl draw_char, 1, 2, 0x0402, '0'      ; _R__ ___G_
-        cdecl draw_char, 2, 2, 0x0212, '1'      ; __G_ A__G_
-        cdecl draw_char, 3, 2, 0x0212, '_'      ; __G_ A__G_
+        ; ** フォントをを印字
+        cdecl draw_font, 63, 13
 
         jmp $
 
@@ -65,6 +26,7 @@ FONT:   dd 0                                ; フォントアドレス保持先
 
 %include "modules/protect/vga.s"
 %include "modules/protect/draw_char.s"
+%include "modules/protect/draw_font.s"
 
 ;********************************************************************************
 ; パディング(8kB)
---
+++ src/modules/protect/draw_font.s
@@ -0,0 +1,53 @@
+;********************************************************************************
+; void draw_font(col, row)
+;********************************************************************************
+draw_font:
+;**** スタックフレームの構築 **** 
+                            ;    +12| 行                          
+                            ;     +8| 列
+                            ;     +4| EIP (caller)
+        push ebp            ; EBP  0| EBP (old)
+        mov ebp, esp
+
+;**** レジスタの保存 **** 
+        push eax
+        push ebx
+        push ecx
+        push esi
+        push edi
+
+;**** 処理の開始 ****
+        mov esi, [ebp + 8]      ; 列
+        mov edi, [ebp + 12]     ; 行
+
+        mov ecx, 0
+.LOOP:
+        cmp ecx, 256
+        jae .END
+
+        mov eax, ecx
+        and eax, 0x0F           ; 
+        add eax, esi
+
+        mov ebx, ecx
+        shr ebx, 4
+        add ebx, edi
+
+        cdecl draw_char, eax, ebx, 0x07, ecx
+
+        inc ecx
+        jmp .LOOP
+.END:
+        jmp $
+
+;**** レジスタの復帰 **** 
+        pop edi
+        pop esi
+        pop ecx
+        pop ebx
+        pop eax
+
+;**** スタックフレームの破棄 ****
+        mov esp, ebp
+        pop ebp
+        ret
