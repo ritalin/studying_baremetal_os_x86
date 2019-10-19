492efdccb9eea2a74627e48a5374053cb0e60a93
Ritalin <kazu.ktz.tamura@gmail.com>
2019-10-19 21:34:30 +0900

    [Fix] RTCから時刻を取得するよう修正した

--- src/modules/protect/rtc.s
+++ src/modules/protect/rtc.s
@@ -1,5 +1,5 @@
 ;********************************************************************************
-; void int get_rtc_time(out time)
+; int get_rtc_time(out time)
 ;********************************************************************************
 get_rtc_time:
 ;**** スタックフレームの構築 **** 
@@ -9,13 +9,30 @@ get_rtc_time:
         mov ebp, esp
 
 ;**** レジスタの保存 **** 
-        push edi
+        push eax
+        push ebx
 
 ;**** 処理の開始 ****
-    mov edi, [ebp + 8]
-    mov [edi + 0], byte 44
-    mov [edi + 1], byte 18
-    mov [edi + 2], byte 13
+        mov al, 0x04
+        out 0x70, al        ; RTC RAMの読み取り先を指示 (Hour)
+        in al, 0x71         ; 時刻パートを読み込む
+
+        shl eax, 8          ; AHに読み込んだ時刻パートを退避
+
+        mov al, 0x02
+        out 0x70, al            ; RTC RAMの読み取り先を指示 (Min)
+        in al, 0x71             ; 時刻パートを読み込む
+
+        shl eax, 8          ; AHに読み込んだ時刻パートをさらに退避
+
+        mov al, 0x00
+        out 0x70, al            ; RTC RAMの読み取り先を指示 (Sec)
+        in al, 0x71             ; 時刻パートを読み込む
+       
+        and eax, 0x00_FF_FF_FF   ; 下位3Byteをマスク
+
+        mov ebx, [ebp + 8]
+        mov [ebx], eax
 
 ;**** レジスタの復帰 **** 
         pop edi
