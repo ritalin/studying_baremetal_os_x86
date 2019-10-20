59e532ac299671a96eb38fb6ebb36be34167de0e
Ritalin <kazu.ktz.tamura@gmail.com>
2019-10-20 11:25:23 +0900

    [Fix] ハーウェア割り込みにより時刻を更新するよう修正した

--- src/28_int_rtc/kernel.s
+++ src/28_int_rtc/kernel.s
@@ -25,11 +25,17 @@ kernel:
         ; ** PICを初期化する
         cdecl init_pic
 
+        cdecl enable_rtc_int, 0x10              ; 更新サイクル終了割り込み(UIE)を許可する
+
         set_vect 0x00, int_zero_div
         set_vect 0x28, int_rtc
+        
+        ; ** IMR(割り込みマスクレジスタ)の設定
+        outp 0x21, 0b_1111_1011                 ; スレーブPICを有効にする
+        outp 0xA1, 0b_1111_1110                 ; RTCの割り込みを有効にする
 
         sti
-        
+
         ; ** フォントをを印字
         cdecl draw_font, 63, 13
 
@@ -40,9 +46,6 @@ kernel:
         cdecl draw_color_bar, 63, 4
 
 .UPDATE_RTC_TIME:
-        ; ** 直接割り込みハンドラをキック
-        int 0x28
-
         ; ** 時刻を表示する
         mov eax, [RTC_TIME]
         cdecl draw_time, 72, 0, 0x0700, eax
