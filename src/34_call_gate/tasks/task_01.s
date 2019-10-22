7a8fc6fd2070d3704b9ee25cf2f34693b31e46a1
Ritalin <kazu.ktz.tamura@gmail.com>
2019-10-23 00:30:05 +0900

    [WIP] 文字列描画のためにIOポートへのアクセスを求めていたことにより例外が発生していたことを確認

--- src/34_call_gate/tasks/task_01.s
+++ src/34_call_gate/tasks/task_01.s
@@ -9,14 +9,6 @@ task_01:
 ;**** 処理の開始 ****
 
 .LOOP:
-        cdecl draw_str, 63, 0, 0x07, .s0
-
-        ; ** 時刻を表示する
-        mov eax, [RTC_TIME]
-        cdecl draw_time, 72, 0, 0x0700, eax
-
-;        jmp SS_TASK_00:0
-        hlt
         jmp .LOOP
 
 ;**** レジスタの復帰 **** 
