9766a06d92387ca468bafc9474669f9d096f0614
Ritalin <kazu.ktz.tamura@gmail.com>
2019-10-23 21:34:59 +0900

    [Fix] コールゲート外部から渡された引数を適用するよう修正した

--- src/modules/protect/call_gate.s
+++ src/modules/protect/call_gate.s
@@ -12,16 +12,29 @@ call_gate:
         push ebp            ; EBP  0| EBP (old)
         mov ebp, esp
 ;**** レジスタの保存 **** 
+        pusha
+        push ds
+        push es
 
 ;**** 処理の開始 ****
-        cdecl draw_str, 50, 0, 0x02, .s0
+        ; ** セグメントレジスタの初期化
+        mov ax, 0x0010
+        mov ds, ax
+        mov es, ax
 
+        mov eax, dword [ebp + 12]
+        mov ebx, dword [ebp + 16]
+        mov ecx, dword [ebp + 20]
+        mov edx, dword [ebp + 24]
+
+        cdecl draw_str, eax, ebx, ecx, edx
 
 ;**** レジスタの復帰 **** 
+        pop es
+        pop ds
+        popa
         
 ;**** スタックフレームの破棄 ****
         mov esp, ebp
         pop ebp
         retf 4 * 4
-
-.s0: db "By Call Gate"
