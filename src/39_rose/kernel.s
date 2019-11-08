782120fc38876f2f0477d26da8adf2ec0ecd7fc2
Ritalin <kazu.ktz.tamura@gmail.com>
2019-11-09 01:38:28 +0900

    [Fix] draw_line, draw_rectをインクルードした

--- src/39_rose/kernel.s
+++ src/39_rose/kernel.s
@@ -120,6 +120,11 @@ RTC_TIME:
 %include "modules/protect/draw_char.s"
 %include "modules/protect/draw_font.s"
 %include "modules/protect/draw_str.s"
+
+%include "modules/protect/draw_pixel.s"
+%include "modules/protect/draw_line.s"
+%include "modules/protect/draw_rect.s"
+
 %include "modules/protect/draw_color_bar.s"
 %include "modules/protect/draw_time.s"
 %include "modules/protect/itoa.s"
